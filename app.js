e7d5d0a9d70a269f2c384fb476668a14f165a2e8
Anna075 <doroftei.ana02@gmail.com>
2022-05-19 23:10:10 +0300

    Added mad-lib replacement variables to story

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 window.onload = () => {
 
-    let numberOfQuestions = 8;
+    
     let userInputs = [];
     let originlStory = '$<function>{userInputs[0]} suffers from a ${userInputs[1]}. ${userInputs[0]} kept blowing and wiping his ${userInputs[2]}. The nose turned ${userInputs[3]} and his ${userInputs[2]} swelled with ${userInputs[4]}. His/her friend ${userInputs[5]} on him a vessel filled with ${userInputs[6]} and put some hot ${userInputs[7]} into it. As ${userInputs[0]} inhaled the steam, the ${userInputs[1]} subsided slowly. ${userInputs[0]} was relieved.';
 
