6d8b333077d8b2b17656c5778419f18b7e92a495
Anna075 <doroftei.ana02@gmail.com>
2022-05-19 22:50:42 +0300

    Added mad-lib replacement variables to story

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+window.onload = () => {
+
+    
+    let userInputs = [];
+    let originlStory = '$<function>{userInputs[0]} suffers from a ${userInputs[1]}. ${userInputs[0]} kept blowing and wiping his ${userInputs[2]}. The nose turned ${userInputs[3]} and his ${userInputs[2]} swelled with ${userInputs[4]}. His/her friend ${userInputs[5]} on him a vessel filled with ${userInputs[6]} and put some hot ${userInputs[7]} into it. As ${userInputs[0]} inhaled the steam, the ${userInputs[1]} subsided slowly. ${userInputs[0]} was relieved.';
+
+};
+
+/* Tenali Raman suffers from a bad cold. He kept blowing and wiping his nose. The nose turned red and his eyes swelled with tears. His wife brought to him a vessel filled with hot water. She put some herbs into it. As Raman inhaled the steam, the cold subsided slowly. Raman was relieved.';
+that is a true story.
+*/
